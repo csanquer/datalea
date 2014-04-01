64d54dceb239b80689685172f7cbd1aa9960ac84
Charles Sanquer <charles.sanquer@gmail.com>
2014-04-01 16:12:24 +0200

    add XMLDumper unit tests

--- src/CSanquer/FakeryGenerator/Dump/XMLDumper.php
+++ src/CSanquer/FakeryGenerator/Dump/XMLDumper.php
@@ -11,19 +11,98 @@ use CSanquer\FakeryGenerator\Model\Config;
  */
 class XMLDumper extends AbstractDumper
 {
+    /**
+     *
+     * @var \XMLWriter
+     */
+    protected $xml;
+    
+    /**
+     *
+     * @var string
+     */
+    protected $elementName;
+    
     public function initialize(Config $config, $directory)
     {
-
+        $this->setFilename($config, $directory);
+        
+        $this->xml = new \XMLWriter();
+        $this->xml->openUri($this->filename);
+        $this->xml->startDocument('1.0', 'utf-8'); 
+        
+        $this->xml->setIndent(true);
+        $this->xml->setIndentString('    ');
+        
+        $rootName = strtolower(str_ireplace('_', '', $config->getClassName(true)));
+        $this->xml->startElement($rootName);
+        
+        $this->elementName = strtolower($config->getClassNameLastPart());
     }
     
     public function dumpRow(array $row = array())
     {
-
+        $this->dumpXmlElement($this->elementName, $row);
     }
 
+    protected function dumpXmlElement($key, $value) 
+    {
+        $this->xml->startElement(strtolower(preg_replace('/[^a-zA-Z0-9]/', '', $key)));
+        
+        if (is_array($value)) {
+            foreach ($value as $subKey => $subValue) {
+                $this->dumpXmlElement($subKey, $subValue);
+            }
+        } else {
+            //need CDATA
+            if (preg_match('/[<>&]/', $value)) {
+                $this->xml->writeCdata($value);
+            } else {
+                $this->xml->text($value);
+            }
+        }
+        
+        $this->xml->endElement();
+    }
+    
     public function finalize()
     {
+        $this->xml->endElement(); //root element
+        $this->xml->endDocument();
+        $this->xml->flush();
+        
+        return $this->filename;
+        
+        /**
+        $fakeData = $this->getFakeData();
 
+        $name = $this->config->getClassName(true);
+
+        $elementRootName = strtolower(str_ireplace('_', '', $name));
+        $elementName = strtolower($this->config->getClassNameLastPart());
+
+        $root = new \CSanquer\FakeryGenerator\XML\CdataSimpleXMLElement('<?xml version=\'1.0\' encoding=\'utf-8\'?><'.$elementRootName.'s/>');
+
+        foreach ($fakeData as $items) {
+            $element = $root->addChild($elementName);
+            foreach ($items as $column => $value) {
+                $element->addChild(strtolower(preg_replace('/[^a-zA-Z0-9]/', '', $column)), $value);
+            }
+        }
+
+        $file = $dir.DS.$name.'.xml';
+
+        $rootDom = dom_import_simplexml($root);
+        $dom = new DOMDocument('1.0', 'UTF-8');
+        $dom->formatOutput = true;
+        $rootDom = $dom->importNode($rootDom, true);
+        $rootDom = $dom->appendChild($rootDom);
+
+        $dom->save($file);
+
+        return $file;
+        /**/
+        
     }
 
     public function getExtension()
---
+++ tests/CSanquer/FakeryGenerator/Test/Dump/XMLDumperTest.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace CSanquer\FakeryGenerator\Test\Dump;
+
+use CSanquer\ColibriCsv\Dialect;
+use CSanquer\FakeryGenerator\Dump\XMLDumper;
+use CSanquer\FakeryGenerator\Model\Column;
+use CSanquer\FakeryGenerator\Model\Config;
+use CSanquer\FakeryGenerator\Model\Variable;
+use Faker\Factory;
+
+/**
+ * XMLDumperTest
+ *
+ * @author Charles Sanquer <charles.sanquer.ext@francetv.fr>
+ */
+class XMLDumperTest extends DumperTestCase
+{
+    /**
+     * @dataProvider providerDump
+     */
+    public function testDump(Config $config, $generatedValues, $expectedFile)
+    {
+//        $faker = Factory::create($config->getLocale());
+
+        $dumper = new XMLDumper();
+        $dumper->initialize($config, self::$cacheDir);
+
+//        $result = [];
+        foreach ($generatedValues as $row) {
+            $dumper->dumpRow($row);
+        }
+        
+//        for ($i = 0; $i < count($generatedValues); $i++) {
+//            $values = [];
+//            $config->generateVariableValues($faker, $values);
+//            $row = $config->generateColumnValues($values);
+//            $result[] = $row;
+//            $dumper->dumpRow($row);
+//        }
+
+//        var_export($result);
+
+        $filename = $dumper->finalize();
+//        var_dump($filename);
+//        var_dump(file_get_contents($filename));
+        $this->assertFileExists($filename);
+        $this->assertEquals(basename($expectedFile), basename($filename));
+        $this->assertFileEquals(self::$fixtures.'/'.$expectedFile, $filename);
+    }
+
+    public function providerDump()
+    {
+        $config1 = new Config();
+        $config1
+            ->setClassName('Entity\\User')
+            ->setFakeNumber(3)
+            ->setFormats(['csv'])
+            ->setLocale('en_US')
+            ->setSeed(17846134)
+            ->setCsvDialect(Dialect::createUnixDialect())
+            ->setVariables([
+                new Variable('firstname', 'firstName'),
+                new Variable('lastname', 'lastName'),
+                new Variable('emailDomain', 'freeEmailDomain'),
+                new Variable('birthday', 'dateTimeThisCentury', ['Y-m-d']),
+                new Variable('site', 'url'),
+            ])
+            ->setColumns([
+                new Column('person', null, null, [
+                    new Column('name', null, null, [
+                        new Column('firstname', '%firstname%', 'capitalize'),
+                        new Column('lastname', '%lastname%', 'capitalize'),
+                            ]),
+                    new Column('email', '%firstname%.%lastname%@%emailDomain%', 'lowercase'),
+                        ]),
+                new Column('birthday', '%birthday%'),
+                new Column('personal_site', '%site%?foo=<&bar=>'),
+            ]);
+
+        return [
+            #data set #0
+            [
+                $config1,
+                [
+                    [
+                        'person' => [
+                            'name' => [
+                                'firstname' => 'Adolph',
+                                'lastname' => 'McCullough',
+                            ],
+                            'email' => 'adolph.mccullough@yahoo.com',
+                        ],
+                        'birthday' => '1994-05-30',
+                        'personal_site' => 'http://www.rohan.com/?foo=<&bar=>',
+                    ],
+                    [
+                        'person' => [
+                            'name' => [
+                                'firstname' => 'Sebastian',
+                                'lastname' => 'Harvey',
+                            ],
+                            'email' => 'sebastian.harvey@yahoo.com',
+                        ],
+                        'birthday' => '1927-10-02',
+                        'personal_site' => 'http://glover.com/?foo=<&bar=>',
+                    ],
+                    [
+                        'person' => [
+                            'name' => [
+                                'firstname' => 'Norris',
+                                'lastname' => 'Douglas',
+                            ],
+                            'email' => 'norris.douglas@hotmail.com',
+                        ],
+                        'birthday' => '1994-08-12',
+                        'personal_site' => 'http://brown.com/?foo=<&bar=>',
+                    ],
+                ],
+                'XMLDumper/expected/Entity_User.xml'
+            ]
+        ];
+    }
+}
